667d4b5d7b5df41ce0a7649a0c398e1f7ecff080
Justin747 <justin747@gmail.com>
2022-02-09 20:56:52 -0800

    Initial Commit

---
+++ Close Animation/Close_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Close_AnimationApp.swift
+//  Close Animation
+//
+//  Created by Justin747 on 2/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Close_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
