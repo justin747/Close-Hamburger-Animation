78f03ed72eab4613abaeaea281d746ec85747614
Justin747 <justin747@gmail.com>
2022-02-09 21:05:05 -0800

    Close Hamburger Animation
    Close Hamburger Animation

--- Close Animation/ContentView.swift
+++ Close Animation/ContentView.swift
@@ -8,9 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+   
+    @State private var isRotating = false
+    @State private var isHidden    = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack(spacing: 14) {
+            Rectangle()
+                .frame(width: 64, height: 10)
+                .cornerRadius(4)
+                .rotationEffect(.degrees(isRotating ? 48 : 0), anchor: .leading)
+            
+            Rectangle()
+                .frame(width: 64, height: 10)
+                .cornerRadius(4)
+                .scaleEffect(isHidden ? 0 : 1, anchor: isHidden ? .trailing : .leading)
+                .opacity(isHidden ? 0 : 1)
+            
+            Rectangle()
+                .frame(width: 64, height: 10)
+                .cornerRadius(4)
+                .rotationEffect(.degrees(isRotating ? -48 : 0), anchor: .leading)
+        }
+        .onTapGesture {
+            withAnimation(.interpolatingSpring(stiffness: 300, damping: 15)) {
+                isRotating.toggle()
+                isHidden.toggle()
+            }
+        }
     }
 }
 
